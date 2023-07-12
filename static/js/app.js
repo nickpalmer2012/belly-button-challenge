277d1b74201fdc9993e678aea0706ae58a3c6fb9
nickpalmer2012 <nickpalmer2012@gmail.com>
2023-07-12 16:44:49 -0500

    final commit - gauge chart done

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,6 @@ function init() {
 
     // iterate over names list and add each ID to the dropdown menu variable
     namesList.forEach(name => {
-        // console.log(name)
         // select the dropdownMenu variable that was initialized above
         dropdownMenu
         // each string in the "names" object will need to be appended as an <option> element
@@ -26,15 +25,14 @@ function init() {
         .text(name);
     })
   
- // Set up plots to show first sample when page opens
- // Bar graph
+ // Set up plots and Demographic Info box to show display data for first sample when page opens
+ // Bar graph and Bubble Chart
  const firstPlotId = namesList[0];
  chartCreation(firstPlotId);
 
-    
-
+ //Demographic Info Box
+ demographics(firstPlotId); 
 })}
-
 
 // Create function that creates charts using the data selected in the dropdown menu
 function chartCreation(sample) {
@@ -43,7 +41,7 @@ function chartCreation(sample) {
     d3.json(url).then((data) => {
         let samplesArray = data.samples;
 
-        // Get sample_values from selected sample
+        // Get bar chart parameters from api query of the selected ID from dropdown box
 
         // Isolate selected sample from samplesArray by filtering by selected sample ID
         let selectedSample = samplesArray.filter(sampleJSON => sampleJSON.id == sample);
@@ -58,8 +56,8 @@ function chartCreation(sample) {
         // Isolate the otu_labels from the result to a variable
         let otuLabels = result.otu_labels;
         
-        // Set up bar chart parameters
-        let trace1 = [
+        // Set up bar chart parameters and plot
+        let traceBar = [
             {
                 type: 'bar',
                 x: sampleValues.slice(0,10).reverse(),
@@ -68,19 +66,119 @@ function chartCreation(sample) {
                 orientation: 'h'
     
             }];
-        Plotly.newPlot("bar", trace1)
+        // Create layout variable for bar chart
+        let layoutBar = {
+                title: 'Cultures Present in Naval Sample',
+                xaxis: {
+                    title: 'Sample Values'
+                },
+                yaxis: {
+                    title: 'OTU ID'
+                }
+            };
+         // Plot the bar chart within the "bar" div set up in the index.html file  
+         Plotly.newPlot("bar", traceBar, layoutBar);
 
-    
+        // Set up bubble chart parameters for selected sample
+        let traceBubble = [
+            {
+                x: otuIds,
+                y: sampleValues,
+                mode: 'markers',
+                marker: {
+                    size: sampleValues,
+                    colorscale: 'RdBu',
+                    color: otuIds
+                },
+                text: otuLabels
+            }];
+
+        // Create layout variable for bubble chart
+        let layoutBubble = {
+            title: 'Cultures Present in Naval Sample',
+                xaxis: {
+                    title: 'OTU ID'
+                },
+                yaxis: {
+                    title: 'Sample Values'
+                }
+        }
+        // Create plot with trace and layout arrays and put the chart in the "bubble" div set up in the index.html file
+        Plotly.newPlot("bubble", traceBubble, layoutBubble);
+
+        // Gauge chart variables
+        // Get wash frequency from selected sample
+        let metadataArray = data.metadata;
+
+        // Isolate selected sample from metadataArray by filtering by selected sample ID
+        let selectedSampleWash = metadataArray.filter(sampleJSON => sampleJSON.id == sample);
+        let resultWash = selectedSampleWash[0];
+        // Set wash frequency variable for selected sample
+        let washFreq = resultWash.wfreq;
+
+        // Set up gauge chart trace
+        let traceGauge = [
+            {
+                domain: {x: [0,1], y: [0,1]},
+                type: "indicator",
+                title: {text: 'Belly Button Scrub Frequency Per Week'},
+                mode: "gauge+number",
+                value: washFreq,
+                gauge: {
+                    axis: {range: [null, 9]},
+                    bgcolor: "white",
+                    steps: [{range: [0, 1],color: 'ffffcc'},
+                    {range: [1, 2], color: 'e8ffb5'},
+                    {range: [2, 3], color: 'd9ffa6'},
+                    {range: [3, 4], color: 'c9ff96'},
+                    {range: [4, 5], color: 'baff87'},
+                    {range: [5, 6], color: 'abff78'},
+                    {range: [6, 7], color: '9cff69'},
+                    {range: [7, 8], color: '8cff59'},
+                    {range: [8, 9], color: '7dff4a'}],
+                }
+        }];
+
+        // Create gauge within "gauge" division in index file
+        Plotly.newPlot("gauge", traceGauge);
 
     })
 
 }
 
+// Create function that updates the demographic data in the "Demographic Info" box set up in the index file
+function demographics(sample) {
+    // Pull the array of samples from api using d3 and extract demographic info
+    d3.json(url).then((data) => {
+        let metadataArray = data.metadata;
 
+        // Isolate selected sample from metadataArray by filtering by selected sample ID
+        let selectedSample = metadataArray.filter(sampleJSON => sampleJSON.id == sample);
+        let result = selectedSample[0];
+
+        // fetch demographic box element using d3 library
+        let demoBox = d3.select('#sample-metadata');
+
+        //Clear any text that is in the box already
+        demoBox.html("");
+
+        // Slice the results object to individual key:value pairs
+        let slicedResult = Object.entries(result); 
+
+        // Push sample demographics to the demographics box
+        // Loop through the sliced result and append the items to the demographic info box
+        slicedResult.forEach(keyValue => {
+            demoBox.append("h6")
+            .text(`${keyValue[0]} : ${keyValue[1]}`);
+        })
+       
+    })
+}
 
 // Create function that calls the above functions based on the sample selected in dropdown menu
 function optionChanged(updatedSample) {
     chartCreation(updatedSample);
+    demographics(updatedSample);
 }
 
 // Execute entire "init" function on page load
